8b07a487394fe93811c8196268a06258d924d1b2
mrcaption49 <pranavsb49@gmail.com>
2024-07-11 18:10:29 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    name: 'ONEPLUS AIRPODS 2024',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'iPhone 15 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+  name: 'Cannon EOS 80D DSLR Camera',
+      image: '/images/camera.jpg',
+      description:
+        'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      brand: 'Cannon',
+      category: 'Electronics',
+      price: 929.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+    },
+    {
+      name: 'Sony Playstation 4 Pro White Version',
+      image: '/images/playstation.jpg',
+      description:
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      brand: 'Sony',
+      category: 'Electronics',
+      price: 399.99,
+      countInStock: 10,
+      rating: 5,
+      numReviews: 3,
+    },
+]
+
+
+export default products;
---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const port = process.env.PORT || 5000;
+
+
+const __dirname = path.resolve();
+
+connectDB();
+
+const app = express();
+app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
